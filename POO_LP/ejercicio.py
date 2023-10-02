57843217fc3c953f644a54585468b7ca417ef241
cristians02 <cristiandelmar06@gmail.com>
2023-10-02 09:07:37 -0500

    tareitas de alvares  de POO

--- POO_LP/ejercicio.py
+++ POO_LP/ejercicio.py
@@ -54,8 +54,29 @@ avion1.mostrar_informacion()
 
 # haciendo uso de la POO crear un objeto para la edintidad dota 2
 
+class Dota2Entity:
+    def __init__(self, name, role, attack_type):
+        self.name = name
+        self.role = role
+        self.attack_type = attack_type
 
+# Crear un objeto Dota 2
+dota_hero = Dota2Entity("Anti-Mage", "Carry", "Melee")
+
+# Acceder a los atributos del objeto
+print(dota_hero.name)  # Salida: Anti-Mage
+print(dota_hero.role)  # Salida: Carry
+print(dota_hero.attack_type)  # Salida: Melee
 
 
 
 ## TKINTER .- LIBRERIA  DE PYTHON PARA LA CREACION DE  INTERFAZ GRAFICAS 
+#Tkinter es una biblioteca de Python que se utiliza para crear interfaces gráficas de usuario 
+# (GUI, por sus siglas en inglés). Es una herramienta muy útil para desarrollar aplicaciones con 
+# una interfaz de usuario interactiva y atractiva. Tkinter se basa en la biblioteca Tcl/Tk, que es 
+# una biblioteca de herramientas de desarrollo de software para crear interfaces gráficas de usuario. 
+
+#Con Tkinter, puedes crear ventanas, botones, etiquetas, campos de entrada, menús y otros elementos de
+# la interfaz de usuario. También puedes agregar imágenes y gráficos para hacer que tu aplicación sea más
+# visualmente atractiva. Tkinter es fácil de aprender y usar, lo que lo hace ideal para principiantes en
+# el desarrollo de aplicaciones de escritorio.
