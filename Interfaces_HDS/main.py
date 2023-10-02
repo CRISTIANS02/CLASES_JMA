c1bf00be792ebd8eb04a6218bdbe628dba8eb898
cristians02 <cristiandelmar06@gmail.com>
2023-10-02 12:32:49 -0500

    clases de pk inter ejemplos

--- Interfaces_HDS/main.py
+++ Interfaces_HDS/main.py
@@ -1,43 +1,43 @@
-# # EJERCICIO N°1
-# import tkinter as tk
+# EJERCICIO N°1
+import tkinter as tk
 
-# # Crear una ventana
-# ventana = tk.Tk()
+# Crear una ventana
+ventana = tk.Tk()
 
-# # Personalizar la ventana
-# ventana.title("Mi Aplicación")
-# ventana.geometry("400x300")
+# Personalizar la ventana
+ventana.title("Mi Aplicación")
+ventana.geometry("400x300")
 
-# # Agregar elementos a la ventana
-# etiqueta = tk.Label(ventana, text="¡Hola, Tkinter!")
-# etiqueta.pack()
+# Agregar elementos a la ventana
+etiqueta = tk.Label(ventana, text="¡Hola, Tkinter!")
+etiqueta.pack()
 
-# # Ejecutar el bucle principal de la ventana
-# ventana.mainloop()
+# Ejecutar el bucle principal de la ventana
+ventana.mainloop()
 
 
 
-# #EJEMPLO N° 2
-# import tkinter as tk
-# from tkinter import messagebox
+#EJEMPLO N° 2
+import tkinter as tk
+from tkinter import messagebox
 
-# # Función para mostrar un mensaje cuando se hace clic en el botón
-# def mostrar_mensaje():
-#     messagebox.showinfo("Mensaje", "¡HELLO , SAIRE!")
+# Función para mostrar un mensaje cuando se hace clic en el botón
+def mostrar_mensaje():
+    messagebox.showinfo("Mensaje", "¡HELLO , SAIRE!")
 
-# # Crear una ventana
-# ventana = tk.Tk()
+# Crear una ventana
+ventana = tk.Tk()
 
-# # Personalizar la ventana
-# ventana.title("Ejemplo con Tkinter")
-# ventana.geometry("300x200")
+# Personalizar la ventana
+ventana.title("Ejemplo con Tkinter")
+ventana.geometry("300x200")
 
-# # Crear un botón
-# boton = tk.Button(ventana, text="Haz clickea", command=mostrar_mensaje)
-# boton.pack()
+# Crear un botón
+boton = tk.Button(ventana, text="Haz clickea", command=mostrar_mensaje)
+boton.pack()
 
-# # Ejecutar el bucle principal de la ventana
-# ventana.mainloop()
+# Ejecutar el bucle principal de la ventana
+ventana.mainloop()
 
 
 
