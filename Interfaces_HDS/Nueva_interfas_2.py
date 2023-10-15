eaf009017a897611092650d02e735688b63ea819
cristians02 <cristiandelmar06@gmail.com>
2023-10-15 13:56:33 -0500

    git

--- Interfaces_HDS/Nueva_interfas_2.py
+++ Interfaces_HDS/Nueva_interfas_2.py
@@ -7,13 +7,13 @@ ventana=Tk()
 # Instancio mi primer widget
 Widget_dos=Frame()
 # Usar el metodo para montar el frame
-Widget_uno.grid(row='0',column='0')
-Widget_uno.config(width='100'),height='100'
-Widget_uno.config(bg='yellow')
-# El metodo grid recibe dos parametros el numero de la columna y el numero de la fila de la creacion  del segundo widget
-Widget_dos=Frame()  
-Widget_dos.grid(row='2',column='0')
-Widget_uno.config(width='100'),height='100'
-Widget_uno.config(bg='purple')
+# Widget_uno.grid(row='0',column='0')
+# Widget_uno.config(width='100'),height='100'
+# Widget_uno.config(bg='yellow')
+# # El metodo grid recibe dos parametros el numero de la columna y el numero de la fila de la creacion  del segundo widget
+# Widget_dos=Frame()  
+# Widget_dos.grid(row='2',column='0')
+# Widget_uno.config(width='100'),height='100'
+# Widget_uno.config(bg='purple')
 # Usar el metodo de loop para que la ventana permanesca abierta
 ventana.mainloop()
