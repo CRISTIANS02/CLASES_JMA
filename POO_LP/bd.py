19afb78b70ac47dbf6eefc81e539df92e6f74d8d
cristians02 <cristiandelmar06@gmail.com>
2023-10-24 12:07:27 -0500

    metodos de  productos

---
+++ POO_LP/bd.py
@@ -0,0 +1,134 @@
+   
+tiendas = [
+#tienda → 1
+    {  
+        'ruc': '10714504431',
+        'nombre': 'The Brothers', 
+        'categoria': ['abarrotes', 'bodega'],
+        'horario_atencion': 
+            {
+                'turno_mañana': '7am-12m',
+                'turno_tarde': '2pm-8pm'}, 
+        'gerente': 'china'
+    },
+#tienda → 2
+    {
+        'ruc': '12345678901',
+        'nombre': 'PharmaPlus',
+        'categoria': ['farmacia'],
+         'horario_atencion':
+             {
+                 'turno_mañana': '8am-1pm',
+                 'turno_tarde': '3pm-9pm'
+             },
+         'gerente': 'edwin'
+    },
+#tienda → 3
+    {
+        'ruc': '98765432109',
+        'nombre': 'AyacuChano', 
+        'categoria': 
+            [
+                'abarrotes',
+                'bodega', 
+                'restaurant'
+            ], 
+        'horario_atencion':
+            {
+                'turno_mañana': '9am-1pm',
+                'turno_tarde': '4pm-10pm'
+            },
+         'gerente': 'china'
+    },
+#tienda → 4
+    {
+        'ruc':'10236545896', 
+        'nombre': 'IntiPharmha',
+        'categoria':
+            [
+                'farmacia'
+            ], 
+        'horario_atencion':
+            {
+                'turno_mañana':'9am-2pm', 
+                'turno_tarde':'4pm-9pm'
+            }, 
+        'gerente':'nadine'
+    },
+#tienda → 5
+    {
+        'ruc':'10258963478',
+     'nombre': 'Bodega Centralithon', 
+     'categoria':
+         [
+             'bodega'
+        ], 
+     'horario_atencion':
+         {
+             'turno_mañana':'8am-1pm',
+             'turno_tarde':'2pm-8pm'
+         }, 
+         'gerente':'manuel'
+    },
+#tienda → 6
+    {
+        'ruc':'10214563201',
+        'nombre': 'Minimarket Don Pepe',
+        'categoria':
+            [
+                'abarrotes'
+            ],
+        'horario_atencion':
+         {
+             'turno_mañana':'7am-12m',
+             'turno_tarde':'1pm-9pm'
+         }, 'gerente':'china'
+    },
+#tienda → 7
+    {
+        'ruc':'10896325471', 
+        'nombre': 'Restaurante Sabor del Mar', 
+        'categoria':['restaurant'], 
+        'horario_atencion':
+            {
+                'turno_mañana':'11am-4pm', 
+                'turno_tarde':'6pm-11pm'
+            },
+        'gerente':'edwin'
+    }, 
+#tienda → 8
+    {
+        'ruc':'10236587452', 
+        'nombre': 'Farmacia Santa Lucia',
+        'categoria':['farmacia'], 
+        'horario_atencion':
+            {
+                'turno_mañana':'10am-2pm', 
+                'turno_tarde':'5pm-10pm'
+            },
+        'gerente':'nadine'
+    },
+#tienda → 19
+    {
+        'ruc':'10289561743', 
+        'nombre': 'Minimarket Abierto 24/7', 
+        'categoria':['abarrotes', 'bodega'], 
+        'horario_atencion':
+            {
+                'turno_continuo':'0am-11:59pm'
+            },
+        'gerente':'christian'
+    },
+#tienda →10
+ {
+        'ruc':'10289581740', 
+        'nombre': 'The 24/7', 
+        'categoria':['abarrotes', 'bodega'], 
+        'horario_atencion':
+            {
+                'turno_mañana':'7am-12m',
+                'turno_tarde':'1pm-9pm'
+            },
+        'gerente':'nadine'
+    }
+]
