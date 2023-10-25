593c7a0127866cd485b81f4841d3813f1546cfbc
cristians02 <cristiandelmar06@gmail.com>
2023-10-25 11:49:25 -0500

    revicion actualizada de ejercicio 25/10/2023

---
+++ POO_LP/login/bdd.py
@@ -0,0 +1,9 @@
+# crear una lista de objetos
+bjt=[{},{}]
+# cada objeto tendra los siguientes datos de una persona
+#nombre,edad,f_nacimiento,dni,usuario,password
+
+bjt=[{'dni':12345678,
+      'nombre':'moises',
+      'f_nacimiento':28/10/2023,
+      },{}]
---
+++ POO_LP/login/main.py
@@ -0,0 +1,7 @@
+# importar mi base de datos
+from bd import * # la variable usuarios de mi bd estara disponible en este archivo
+#crear clase para usurio
+# esta clase tendra los siguientes metodos
+#actualizar edad del usuario
+#verificar si el usuario esta registrado o existe en mis registros
+# validar usuario y pasword
