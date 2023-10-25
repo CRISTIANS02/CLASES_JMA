acabdb70742fd769f39353abe145920b66afdb56
cristians02 <cristiandelmar06@gmail.com>
2023-10-25 13:02:33 -0500

    loguin

--- POO_LP/login/bdd.py
+++ POO_LP/login/bdd.py
@@ -1,9 +1,48 @@
 # crear una lista de objetos
-bjt=[{},{}]
+
 # cada objeto tendra los siguientes datos de una persona
 #nombre,edad,f_nacimiento,dni,usuario,password
 
-bjt=[{'dni':12345678,
-      'nombre':'moises',
-      'f_nacimiento':28/10/2023,
-      },{}]
+usuarios = [
+  {
+     'dni': 12345678,
+     'nombre': 'Moises',
+     'f_nacimiento': '28/10/2023', 
+     'edad': '',
+     'usuario': 71450443,
+     'password': '71450443'
+  },
+  {
+     'dni': 87654321,
+     'nombre': 'Maria',
+     'f_nacimiento': '25/12/2003',
+     'edad': '',
+     'usuario': 98765432,
+     'password': 'qwerty123'
+  },
+  {
+     'dni': 45678912,
+     'nombre': 'Pedro',
+     'f_nacimiento': '12/1/2000',
+     'edad': '',  
+     'usuario': 159357,
+     'password': 'contrasena654'
+  },
+  {
+     'dni': 25836974,
+     'nombre': 'Karla',
+     'f_nacimiento':'05/06/1999',
+     'edad': '',
+     'usuario': 963258,
+     'password': 'kl12345678'
+  },
+  {
+     'dni': 15963248,
+     'nombre': 'Eduardo',
+     'f_nacimiento': '08/08/2001',
+     'edad': '',
+     'usuario': 741852,
+     'password': 'edu123'
+  }
+]
+print(usuarios)
--- POO_LP/login/main.py
+++ POO_LP/login/main.py
@@ -1,7 +1,88 @@
 # importar mi base de datos
-from bd import * # la variable usuarios de mi bd estara disponible en este archivo
+#from bdd import * # la variable usuarios de mi bd estara disponible en este archivo
 #crear clase para usurio
 # esta clase tendra los siguientes metodos
 #actualizar edad del usuario
 #verificar si el usuario esta registrado o existe en mis registros
 # validar usuario y pasword
+
+# crear una lista de 5 objetos donde cada objeto tendra los siguientes 
+# datos de una persona. nombre,edad,f_nacimiento,dni,usuario,password
+# usuarios[{
+#      'dni': 12345678,
+#      'nombre': 'Moises',
+#      'f_nacimiento': '28/10/2023', 
+#      'edad': '',
+#      'usuario': 71450443,
+#      'password': '71450443'
+#      }]
+
+# 1.actualizar edad del usuario
+# 2.verificar si el usuario esta registrado o existe en mis registros
+# 3.validar usuario y pasword
+
+#1
+import datetime
+import random
+
+usuarios = []
+
+for i in range(5):
+  usuarios.append({
+    'dni': random.randint(10000000,99999999),
+    'nombre': 'Usuario ' + str(i+1),
+    'f_nacimiento': str(datetime.date(random.randint(1950,2005), random.randint(1,12), random.randint(1,28))),
+    'edad': '',
+    'usuario': random.randint(100000,999999),
+    'password': 'password'+str(i)
+  })
+
+print(usuarios)
+
+# 1. Actualizar edades
+from datetime import datetime
+for usuario in usuarios:
+  fecha_nac = datetime.strptime(usuario['f_nacimiento'], '%Y-%m-%d')
+  usuario['edad'] = int((datetime.now() - fecha_nac).days / 365) 
+
+print(usuarios)
+
+# 2. Verificar si usuario existe
+dni_buscar = usuarios[3]['dni']  
+print(any(u['dni'] == dni_buscar for u in usuarios))
+
+# 3. Validar usuario y contraseña
+user_validar = usuarios[4]['usuario']
+pass_validar = usuarios[4]['password']
+print(any(u['usuario'] == user_validar and u['password'] == pass_validar for u in usuarios))
+
+
+
+
+
+
+
+#2
+
+
+
+
+
+# # actualizacion de edad
+
+# from datetime import *
+# from bdd import *
+# for usuario in usuarios:
+#   fecha_nac = datetime.strptime(usuario['f_nacimiento'], '%d/%m/%Y')
+#   usuario['edad'] = int((datetime.now() - fecha_nac).days / 365)
+
+# print(usuarios)
+
+# # Verificar usuario existente
+# dni_buscar = 12345678
+# print(any(u['dni'] == dni_buscar for u in usuarios))
+
+# # Validar usuario y contraseña
+# dni_validar = 98765432 
+# password_validar = 'qwerty123'
+# #print(any(u['dni'] == dni_validar and u['password'] == password_validar for u in usuarios))
