d838c22e7f6fb8eaeb1d6d7ba874d2c336609e56
cristians02 <cristiandelmar06@gmail.com>
2023-11-08 11:37:55 -0500

    rich

--- POO_LP/REPASO_POO/main.py
+++ POO_LP/REPASO_POO/main.py
@@ -1,26 +1,61 @@
-from rich import print
-from rich.markdown import Markdown
-#
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-#titulo
-edad=20
-respuesta='es mayor de edad'if edad>17 else ' es  menor d edad'
-texto="""
-#parrafo
-'''bash
-git commit -m "titulo" -m "cuerpo del commit"
-#comentario
-'''
-*lista
-*lista
-> informacion valiosa
-{}
-""".format(respuesta)
-mostrar_mark=Markdown(texto)
-print(respuesta)
+# REPASO PARA  DE POGRAMACION ORIENTADA A OBJETOS
+class Perro:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from rich import print
+# from rich.markdown import Markdown
+# #
+# # 
+# # 
+# # 
+# # 
+# # 
+# # 
+# # 
+# #titulo
+# edad=20
+# respuesta='es mayor de edad'if edad>17 else ' es  menor d edad'
+# texto="""
+# #parrafo
+# '''bash
+# git commit -m "titulo" -m "cuerpo del commit"
+# #comentario
+# '''
+
+# *lista
+# *lista
+# > informacion valiosa
+# {}
+# """.format(respuesta)
+# mostrar_mark=Markdown(texto)
+# print(respuesta)
