2fdbd0fffc7330bea68b7feb5e8727c38161a77e
cristians02 <cristiandelmar06@gmail.com>
2023-11-24 13:21:16 -0500

    MI APP CONCLUIDA

---
+++ Primera_App/funciones.py
@@ -0,0 +1,62 @@
+from tkinter import *
+from tkinter.messagebox import *
+
+def f_limpiar(ventana):
+    ventana.nombre_texto.delete(0,END)
+    ventana.apellidos_texto.delete(0,END)
+    ventana.celular_texto.delete(0,END)
+
+    ventana.nombre_texto.focus()
+
+def f_nuevo(ventana):
+    nombre=ventana.nombre_texto.get()
+    apellido=ventana.apellidos_texto.get()
+    celular=ventana.celular_texto.get()
+    ventana.tabla_datos.insert("",END,text=nombre,values=(apellido,celular))
+    showinfo(title="Nuevo",message="Nuevo contacto agregado")
+    f_limpiar(ventana)
+
+def f_eliminar(ventana):
+    item_seleccionado = ventana.tabla_datos.selection()
+    print(item_seleccionado)
+    if item_seleccionado:
+        ventana.tabla_datos.delete(item_seleccionado)
+        showwarning(title="ELIMINAR",message="Registro elimnado")
+        f_limpiar(ventana)
+    else:
+        print("Selecciona una fila para eliminar.")
+
+def f_actualizar(ventana):
+    if ventana.nombre_texto.get()=="":
+        showerror(title="SIN DATOS",message="Eres huevon no hay nada para actualizar")
+    else:
+        nombre=ventana.nombre_texto.get()
+        apellidos=ventana.apellidos_texto.get()
+        celular=ventana.celular_texto.get()
+        elem_actualizar=ventana.tabla_datos.selection()
+        mensaje=askyesno(title="Actualizar",message="Estas seguro que deseas actualizar")
+        if mensaje == True:
+            f_limpiar(ventana)
+            ventana.tabla_datos.selection_remove(elem_actualizar)
+            return ventana.tabla_datos.item(elem_actualizar,text=nombre,values=(apellidos,celular))
+        else:
+            showinfo(title="NO ACTUALIZO",message="No se actualizo ningun registro")
+            f_limpiar(ventana)
+            ventana.tabla_datos.selection_remove(elem_actualizar)
+
+def f_dobleClick(ventana,event):
+    elem_actualizar=ventana.tabla_datos.selection()
+    captura_datos=ventana.tabla_datos.item(elem_actualizar)
+    mensaje=askyesno(title="ACTUALIZAR",message="Desea actualizar los datos")
+    if mensaje == True:
+        nombre=captura_datos["text"]
+        apellidos=captura_datos["values"][0]
+        celular=captura_datos["values"][1]
+        ventana.nombre_texto.insert(0,nombre)
+        ventana.apellidos_texto.insert(0,apellidos)
+        ventana.celular_texto.insert(0,celular)
+        ventana.tabla_datos.selection_remove(elem_actualizar)
+    else:
+        showinfo(title="ACTUALIZAR",message="Ningun registro seleccionado para actualizar")
+        ventana.tabla_datos.selection_remove(elem_actualizar)
+    
--- Primera_App/interfaz.py
+++ Primera_App/interfaz.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 from config import *
+from funciones import *
 
 class InterfazApp(Tk):
     def __init__(self):
@@ -19,67 +20,55 @@ class InterfazApp(Tk):
 
     def construir_widget(self):
         #CAJITA DE TEXTOS
-        
         self.cajas_textos=LabelFrame(self,text="Cajas de texto",width=150,height=360,bg=COLOR_FONDO_PRIMARIO,fg="white",font=("arial",12),relief=FLAT,pady=60)
         self.cajas_textos.grid(row=0,column=0,pady=20,padx=20)
-        
         #caja para capturar el nombre
         self.label_nombre=Label(self.cajas_textos,text="Nombres",bg=COLOR_FONDO_PRIMARIO,fg="white",font=("Roboto",10)).pack(pady=10)
         self.nombre_texto=Entry(self.cajas_textos,bd=0,width=12,font=("Arial",14))
         self.nombre_texto.pack()
-        
         #caja para capturar el apellido
         self.label_apellido=Label(self.cajas_textos,text="Apellidos",bg=COLOR_FONDO_PRIMARIO,fg="white",font=("Roboto",10)).pack(pady=10)
         self.apellidos_texto=Entry(self.cajas_textos,bd=0,width=12,font=("Arial",14))
         self.apellidos_texto.pack()
-        
         #caja para capturar el Nro de celular
         self.label_celular=Label(self.cajas_textos,text="Nro Celular",bg=COLOR_FONDO_PRIMARIO,fg="white",font=("Roboto",10)).pack(pady=10)
         self.celular_texto=Entry(self.cajas_textos,bd=0,width=12,font=("Arial",14))
         self.celular_texto.pack()
         #FIN CAJITA DE TEXTOS
-        
         #CAJITA DE BOTONES
         self.cajas_botones=LabelFrame(self,text="Cajas de botones",width=150,height=430,bg=COLOR_FONDO_PRIMARIO,fg="white",font=("arial",12),relief=FLAT,pady=60)
         self.cajas_botones.grid(row=0,column=1,pady=20,padx=20)
-        
         #boton nuevo
-        self.nuevo=Button(self.cajas_botones,text="Nuevo",bg=COLOR_BOTON,fg="White",relief=FLAT,bd=0,width=20,height=2,font=("Arial",10)).pack(pady=10)
-        
+        self.nuevo=Button(self.cajas_botones,command=lambda:f_nuevo(self),text="Nuevo",bg=COLOR_BOTON,fg="White",relief=FLAT,bd=0,width=20,height=2,font=("Arial",10)).pack(pady=10)
         #boton actualizar
-        self.actualizar=Button(self.cajas_botones,text="Actualizar",bg=COLOR_BOTON,fg="White",relief=FLAT,bd=0,width=20,height=2,font=("Arial",10)).pack(pady=10)
-        
+        self.actualizar=Button(self.cajas_botones,command=lambda: f_actualizar(self),text="Actualizar",bg=COLOR_BOTON,fg="White",relief=FLAT,bd=0,width=20,height=2,font=("Arial",10)).pack(pady=10)
         #boton eliminar
-        self.eliminar=Button(self.cajas_botones,text="Eliminar",bg=COLOR_BOTON,fg="White",relief=FLAT,bd=0,width=20,height=2,font=("Arial",10)).pack(pady=10)
-        
+        self.eliminar=Button(self.cajas_botones,command=lambda:f_eliminar(self),text="Eliminar",bg=COLOR_BOTON,fg="White",relief=FLAT,bd=0,width=20,height=2,font=("Arial",10)).pack(pady=10)
         #boton cancelar
-        self.cancelar=Button(self.cajas_botones,text="Cancelar",bg=COLOR_BOTON,fg="White",relief=FLAT,bd=0,width=20,height=2,font=("Arial",10)).pack(pady=10)
+        self.cancelar=Button(self.cajas_botones,command=lambda:f_limpiar(self),text="Cancelar",bg=COLOR_BOTON,fg="White",relief=FLAT,bd=0,width=20,height=2,font=("Arial",10)).pack(pady=10)
         #FIN CAJITA DE BOTONES
-        
-        
-        # TABLA DE DATOS
-        self.cajas_datos=LabelFrame(self,text="Cajas de Datos",width=600,height=430,bg=COLOR_FONDO_PRIMARIO,fg="white",font=("arial",12),relief=FLAT,pady=60)
-        self.cajas_datos.grid(row=0,column=2,pady=20,padx=20)
-        
-        #TABLA
-        self.tabla_datos=ttk.Treeview(self.cajas_datos,columns=("#1","#2"))
-        self.tabla_datos.column("#0",width=60)
-        self.tabla_datos.column("#1",width=60)
-        self.tabla_datos.column("#2",width=60)
-        
+        #TABLA DE DATOS
+        self.caja_datos=LabelFrame(self,text="Caja de Datos",width=600,height=360,bg=COLOR_FONDO_PRIMARIO,fg="white",font=("arial",12),relief=FLAT,pady=60)
+        self.caja_datos.grid(row=0,column=2,pady=20,padx=20)
+
+        #tabla
+        self.tabla_datos=ttk.Treeview(self.caja_datos,columns=("#1","#2"))
+        self.tabla_datos.column("#0",width=40)
+        self.tabla_datos.column("#1",width=120)
+        self.tabla_datos.column("#2",width=40)
+
         self.tabla_datos.heading("#0",text="Nombres")
         self.tabla_datos.heading("#1",text="Apellidos")
         self.tabla_datos.heading("#2",text="Celular")
-        
         alumnitos=[
-            ("mochi","peñadira","965321478"),
-            ("yadira","madafiel","32146987"),
-            ("maria","de jory","632599741"),
-            ("nadine","guadalupe","92587415")
+            ("moises","peñadira","564646441"),
+            ("tarantula","medafiel","987654321"),
+            ("maria","de jory","9877854321"),
+            ("nadine","guadalupe","98888654321")
         ]
-        for nombre,apellido,celular in alumnitos:
-            self.tabla_datos.insert("",END,text=nombre,values=(apellido,celular))
+        for nom,ape,cel in alumnitos:
+            self.tabla_datos.insert("",END,text=nom,values=(ape,cel))
         
-        
+        self.tabla_datos.bind("<Double-1>",lambda event:f_dobleClick(self,event))
         self.tabla_datos.place(x=0,y=0,width=400,height=600)
-        # FIN TABLA DE DATOS
+        #FIN DE TABLA DE DATOS
